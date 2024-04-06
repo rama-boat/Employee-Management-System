c061ca69dcd301fc0b5df020ace78c82d70eeb58
EwuramaBoateng-Yeboah <151070805+EwuramaBoateng-Yeboah@users.noreply.github.com>
2024-04-06 20:46:10 +0000

    db

--- db/EmployeeDB.sql
+++ db/EmployeeDB.sql
@@ -1,54 +1,228 @@
--- Drop the database if it exists
-DROP DATABASE IF EXISTS EmployeeDB;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 06, 2024 at 04:46 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
--- Create the database
-CREATE DATABASE EmployeeDB;
+DROP DATABASE IF EXISTS employeedb;
+CREATE DATABASE employeedb;
+USE employeedb;
 
--- Switch to the newly created database
-USE EmployeeDB;
-
--- Create Employees table
-CREATE TABLE People (
-    Pid INT AUTO_INCREMENT PRIMARY KEY,
-    RoleID INT NOT NULL,
-    FirstName VARCHAR(255) NOT NULL,
-    LastName VARCHAR(255) NOT NULL,
-    DateOfEmployment DATE NOT NULL,
-    Phone VARCHAR(30) NOT NULL,
-    DateOfBirth DATE NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    passwd varchar(255) NOT NULL
-
-);
-
--- Create LeaveRequests table
-CREATE TABLE LeaveRequests (
-    RequestID INT AUTO_INCREMENT PRIMARY KEY,
-    PID INT NOT NULL,
-    LeaveDescription TEXT NOT NULL,
-    StartDate DATE NOT NULL,
-    EndDate DATE NOT NULL,
-    StatusOfLeave ENUM('Pending', 'Approved', 'Rejected') NOT NULL,
-    Comments TEXT NOT NULL,
-    FOREIGN KEY (PID) REFERENCES People(Pid)
-);
-
--- Create AttendanceRecords table
-CREATE TABLE AttendanceRecords (
-    RecordID INT AUTO_INCREMENT PRIMARY KEY,
-    PID INT NOT NULL,
-    WorkingDate DATE NOT NULL,
-    ClockInTime TIME NOT NULL,
-    ClockOutTime TIME NOT NULL,
-    FOREIGN KEY (PID) REFERENCES People(Pid)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-CREATE TABLE Role (
-  RoleID INT NOT NULL,
-  Rname VARCHAR(255) NOT NULL
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO Role (RoleID, Rname) VALUES
-(1, "admin"),
-(2, "standard");
+--
+-- Database: `employeedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendancerecords`
+--
+
+CREATE TABLE `attendancerecords` (
+  `RecordID` int(11) NOT NULL,
+  `EID` int(11) NOT NULL,
+  `WorkingDate` date NOT NULL,
+  `ClockInTime` time NOT NULL,
+  `ClockOutTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendancerecords`
+--
+
+INSERT INTO `attendancerecords` (`RecordID`, `EID`, `WorkingDate`, `ClockInTime`, `ClockOutTime`) VALUES
+(4, 3, '2024-04-06', '07:18:00', '13:19:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `EmpID` int(11) NOT NULL,
+  `RoleID` int(11) NOT NULL,
+  `FirstName` varchar(255) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `Age` int(11) NOT NULL,
+  `Address` varchar(255) NOT NULL,
+  `DateOfBirth` date NOT NULL,
+  `Phone` varchar(30) NOT NULL,
+  `Email` varchar(255) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Department` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` (`EmpID`, `RoleID`, `FirstName`, `LastName`, `Age`, `Address`, `DateOfBirth`, `Phone`, `Email`, `Password`, `Department`) VALUES
+(2, 1, 'Delali', 'Segbor', 24, 'Cfc Estates', '2000-06-08', '0209985215', 'delali.segbor@ashesi.edu.gh', '$2y$10$t5J5t4czgsJ7IZs1mIMHEe9GQTE7MPY3QpqOdtR.SqyimPHwKd3Gq', 'Finance'),
+(3, 2, 'Boatemaa', 'Okrah', 25, '1 University Avenue', '2003-04-06', '0594567899', 'boatemaa.okrah@ashesi.edu.gh', '$2y$10$FgngfW1/li8N7c03trIPS.j4Aa3Lc51aiyOTmBIU0QaT4ppFOGZV.', 'HR');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leaverequests`
+--
+
+CREATE TABLE `leaverequests` (
+  `RequestID` int(11) NOT NULL,
+  `EID` int(11) NOT NULL,
+  `LeaveDescription` text NOT NULL,
+  `StartDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `StatusOfLeave` enum('Pending','Approved','Rejected') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `leaverequests`
+--
+
+INSERT INTO `leaverequests` (`RequestID`, `EID`, `LeaveDescription`, `StartDate`, `EndDate`, `StatusOfLeave`) VALUES
+(4, 3, 'sick leave', '2024-04-08', '2024-04-12', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `RoleID` int(11) NOT NULL,
+  `Rname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`RoleID`, `Rname`) VALUES
+(1, 'admin'),
+(2, 'employee');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statusofleave`
+--
+
+CREATE TABLE `statusofleave` (
+  `ID` int(11) NOT NULL,
+  `Status` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `statusofleave`
+--
+
+INSERT INTO `statusofleave` (`ID`, `Status`) VALUES
+(1, 'Pending'),
+(2, 'Approved'),
+(3, 'Rejected');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `attendancerecords`
+--
+ALTER TABLE `attendancerecords`
+  ADD PRIMARY KEY (`RecordID`),
+  ADD KEY `attendancerecords_ibfk_1` (`EID`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`EmpID`),
+  ADD KEY `employee_ibfk_1` (`RoleID`);
+
+--
+-- Indexes for table `leaverequests`
+--
+ALTER TABLE `leaverequests`
+  ADD PRIMARY KEY (`RequestID`),
+  ADD KEY `leaverequests_ibfk_1` (`EID`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`RoleID`);
+
+--
+-- Indexes for table `statusofleave`
+--
+ALTER TABLE `statusofleave`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attendancerecords`
+--
+ALTER TABLE `attendancerecords`
+  MODIFY `RecordID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `EmpID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `leaverequests`
+--
+ALTER TABLE `leaverequests`
+  MODIFY `RequestID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `statusofleave`
+--
+ALTER TABLE `statusofleave`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `attendancerecords`
+--
+ALTER TABLE `attendancerecords`
+  ADD CONSTRAINT `attendancerecords_ibfk_1` FOREIGN KEY (`EID`) REFERENCES `employee` (`EmpID`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `employee`
+--
+ALTER TABLE `employee`
+  ADD CONSTRAINT `employee_ibfk_1` FOREIGN KEY (`RoleID`) REFERENCES `role` (`RoleID`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `leaverequests`
+--
+ALTER TABLE `leaverequests`
+  ADD CONSTRAINT `leaverequests_ibfk_1` FOREIGN KEY (`EID`) REFERENCES `employee` (`EmpID`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
