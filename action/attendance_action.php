1f8ed4bb80dda3c626045ae268d1a5a293737b11
rama_boat <ewurama.yeboah@ashesi.edu.gh>
2024-04-09 15:20:56 +0000

    update

--- action/attendance_action.php
+++ action/attendance_action.php
@@ -15,13 +15,12 @@ if(isset($_POST["submit"])){
     //write a query
     $sql_query = "INSERT INTO attendancerecords (EID,WorkingDate,ClockInTime,ClockOutTime) VALUES ('$h1','$work_date','$clock_in','$clock_out')";
     
-   
+    var_dump($sql_query);
+    exit();
 
         
     // check if query worked
     if ($con->query($sql_query)) {
-        var_dump($sql_query);
-        exit();
         echo "Attendance Recorded successfully!";
         header("Location:../view/empAttendance.php");
 
